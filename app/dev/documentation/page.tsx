ddddd7d99acf9062e1ad25aa284ae1985eb73517
NekodaMushi <study.fabien@gmail.com>
2024-04-24 19:53:15 +0200

    small clean for vercel

--- app/dev/documentation/page.tsx
+++ app/dev/documentation/page.tsx
@@ -1,13 +1,6 @@
 import React from "react";
-import { redirect } from "next/navigation";
-import { auth, signOut } from "@/lib/auth/auth";
 
-export const Documentation = async () => {
-  const session = await auth();
-
-  if (!session?.user) {
-    redirect("/api/auth/signin?callbackUrl=/");
-  }
+const Documentation = () => {
   return <div>Documentation</div>;
 };
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from "next";
 // import { Inter } from "next/font/google";
 import "@/assets/styles/globals.css";
 import { jetBrainsMono } from "../fonts";
