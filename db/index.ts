e9dd35d344edb35abbba1e9bcfd9d02b4ba6f653
NekodaMushi <study.fabien@gmail.com>
2024-04-25 20:58:14 +0200

    fixing vercel

--- db/index.ts
+++ db/index.ts
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
-import * as schema from "../notes_tests/schemas/schema";
+import * as schema from "@/db/schema";
 
 // console.log(schema);
 const sql = neon(process.env.DATABASE_URL!);
