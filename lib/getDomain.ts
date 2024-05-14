e2288351f7e027e67b339fa4c77f34ed74d9ce45
NekodaMushi <study.fabien@gmail.com>
2024-05-14 23:09:57 +0200

    vercel

--- lib/getDomain.ts
+++ lib/getDomain.ts
@@ -1,9 +1,14 @@
 export default function getDomain() {
-  const protocol = process.env.NEXT_PUBLIC_VERCEL_ENV === "production" ? "https" : "http"
-
-  const domain = process.env.NEXT_PUBLIC_VERCEL_URL ? process.env.NEXT_PUBLIC_VERCEL_URL : "localhost:3000"
-
-  return `${protocol}://${domain}`
+  return `https://your-vercel-deployment-url.vercel.app`;
 }
 
-// leave localhost until end of development, put vercel link after
+
+// export default function getDomain() {
+//   const protocol = process.env.NEXT_PUBLIC_VERCEL_ENV === "production" ? "https" : "http"
+
+//   const domain = process.env.NEXT_PUBLIC_VERCEL_URL ? process.env.NEXT_PUBLIC_VERCEL_URL : "localhost:3000"
+
+//   return `${protocol}://${domain}`
+// }
+
+// // leave localhost until end of development, put vercel link after
