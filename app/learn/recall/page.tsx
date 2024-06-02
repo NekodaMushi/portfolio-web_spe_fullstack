07d3f21a8ba4732fe4be69f928972e3ef367af3d
NekodaMushi <study.fabien@gmail.com>
2024-06-02 19:47:12 +0200

    Abandonning speed from server side component rendering for now, to allow successful deployemnt on vercel

--- app/learn/recall/page.tsx
+++ app/learn/recall/page.tsx
@@ -1,15 +1,36 @@
+"use client";
+import { useEffect, useState } from "react";
 import RecallOrQuiz from "@/components/recall/client/RecallOrQuiz";
 import Recall from "@/components/recall/server/Recall";
 import { CarouselData } from "@/types/quiz";
 import fetchCards from "@/components/recall/server/action";
+import Spinner from "@/components/ui/spinner";
 
-export default async function RecallServerComponent() {
-  let data: CarouselData = [];
+export default function RecallPage() {
+  const [data, setData] = useState<CarouselData>([]);
+  const [loading, setLoading] = useState(true);
 
-  try {
-    data = await fetchCards(1);
-  } catch (error) {
-    console.error("Failed to fetch data:", error);
+  useEffect(() => {
+    async function loadData() {
+      try {
+        const fetchedData = await fetchCards(1);
+        setData(fetchedData);
+      } catch (error) {
+        console.error("Failed to fetch data:", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    loadData();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <Spinner />
+      </div>
+    );
   }
 
   return (
