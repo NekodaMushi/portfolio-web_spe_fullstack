e0dd970932b5165dac462ba2334908244996d7cd
NekodaMushi <study.fabien@gmail.com>
2024-06-05 13:17:18 +0200

    saving for later

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,40 @@
-import NextAuth from "next-auth";
-import { authConfig } from "./lib/auth/auth";
-import { NextRequest, NextResponse } from "next/server";
+
+  import { NextRequest, NextResponse } from "next/server";
+import { auth } from "./lib/auth/auth";
 import { rateLimiter } from "./lib/utils/chat/rate-limiter";
 
-// NextAuth handler
-export default NextAuth(authConfig).auth;
 
-// Chat limiting rate
-export async function middleware(req: NextRequest) {
+async function chatRateLimitMiddleware(req: NextRequest) {
   const ip = req.ip ?? '127.0.0.1';
 
-  if (req.nextUrl.pathname.startsWith('/api/chat/message')) {
-    try {
-      const { success } = await rateLimiter.limit(ip);
-      if (!success) return new NextResponse('Please slow down.');
-    } catch (error) {
-      return new NextResponse('Something went wrong, please try later.');
+  try {
+    const { success } = await rateLimiter.limit(ip);
+    if (!success) return new NextResponse('Please slow down.');
+  } catch (error) {
+    return new NextResponse('Something went wrong, please try later.');
+  }
+
+  return NextResponse.next();
+}
+
+export async function middleware(req: NextRequest) {
+  const protectedPaths = ["/use/quiz", "/use/chat", "/learn/recall"];
+
+  if (protectedPaths.some(path => req.nextUrl.pathname.startsWith(path))) {
+    const session = await auth();
+    const isLoggedIn = !!session;
+    
+
+    if (!isLoggedIn) {
+      const signInUrl = new URL("/api/auth/signin", req.nextUrl.origin);
+      signInUrl.searchParams.set("callbackUrl", req.nextUrl.href);
+      return NextResponse.redirect(signInUrl);
     }
   }
 
+  if (req.nextUrl.pathname.startsWith('/api/chat/message')) {
+    return chatRateLimitMiddleware(req);
+  }
 
   return NextResponse.next();
 }
