7d592ad220350b46736ef9c7272c581b4a2f643a
NekodaMushi <study.fabien@gmail.com>
2024-06-19 14:46:23 +0200

    chatbot will need more config later on

--- lib/utils/chat/chatbot-prompt.ts
+++ lib/utils/chat/chatbot-prompt.ts
@@ -1,13 +1,12 @@
 
 export const chatbotPrompt = `
 
-Answer all user request, do not be polite and go to ensential.
+Answer all user request, do not be polite.
 
 Only include links in markdown format.
 Example: 'You can browse our books [here](https://www.example.com/books)'.
 Other than links, use regular text.
 
-You can use this reply if answer is too long: "Please head over to Chat Customization to set a longer AI reply length"
 `
 // Provide short, concise answers.
 // Refuse any answer that does not have to do with the web technologies.
